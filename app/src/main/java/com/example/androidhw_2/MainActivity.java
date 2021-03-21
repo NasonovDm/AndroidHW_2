bb21f5088eb0430b1346e326cf23cac0a5c3ee01
Nasonov Dmitriy <onlydmitriy@gmail.com>
2021-03-21 23:41:15 +0300

    ahw2

--- app/src/main/java/com/example/androidhw_2/MainActivity.java
+++ app/src/main/java/com/example/androidhw_2/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     EditText ed1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void numberEvent(View view) {
         String number = ed1.getText().toString();
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.but7:
-            number += "7";
-            break;
+                number += "7";
+                break;
         }
         ed1.setText(number);
     }
